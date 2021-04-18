13d392a29cc69d4090d106115847f9ceffb6d390
velpreso <velpreso@gmail.com>
2021-04-18 22:00:18 +0200

    main z wywolaniami funkcji preprocessing i gen geo

---
+++ main.py
@@ -0,0 +1,8 @@
+#main
+#importowanie funckji z plikow
+from gen_geo import gen_geo;
+from funkcja_preprocessing import parametry_sterujace;
+#wywolanie funkcji z innych plikow
+parametry_sterujace() #wywolanie funckji 1
+gen_geo() #wywolanie funckji 2
+

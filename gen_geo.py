9943823648b898127d1bd5f80820fd74e67d30d5
velpreso <velpreso@gmail.com>
2021-04-18 21:46:58 +0200

    generowanie geografii

---
+++ gen_geo.py
@@ -0,0 +1,12 @@
+import numpy as np
+#funkcja generująca geometrie
+def gen_geo():
+    global n;
+    i=0
+    n=100; #ilosc
+    for i in range(0,n):
+        ;
+
+gen_geo()#wywolanie testowe funkcji
+        
+        
